8ecf49474e73eaccd8ecd9ee35a6c0b6da7ee0bb
AlterRemz <255678841+AlterRemz@users.noreply.github.com>
2026-08-29 09:42:10 +0700

    Feat: add hydro logic dashboard frontend

--- simulator_iot.py
+++ simulator_iot.py
@@ -4,29 +4,38 @@ URL = "http://127.0.0.1:8000/api/sensor-data"
 
 print("=== 🚜 SIMULATOR IoT Hydro Logic ===")
 print("Gunakan alat ini untuk mengirim data dummy ke server FastAPI\n")
+print('Alat ini akan terus meminta input. Ketik "q" untuk berhenti.\n')
 
-sektor = input("Masukkan nama sektor (cth: Sektor A) : ")
-kelembapan = float(input("Masukkan kelembapan tanah (0-100)  : "))
-air = float(input("Masukkan level air cadangan (0-100): "))
+while True:
+    print("\n" + "="*45)
+    sektor = input('Masukkan nama sektor (cth: Sektor A atau "Q" untuk keluar) : ')
 
-payload = {
-    "sektor_lahan": sektor,
-    "kelembapan_tanah": kelembapan,
-    "level_air_cadangan": air
-}
+    if sektor.lower() in ['q', 'exit', 'quit']:
+        print("Mematikan simulator...")
+        break
+    try:
+        kelembapan = float(input("Masukkan kelembapan tanah (0-100)  : "))
+        air = float(input("Masukkan level air cadangan (0-100): "))
 
-print("\nMengirim data ke server...")
+        payload = {
+        "sektor_lahan": sektor,
+        "kelembapan_tanah": kelembapan,
+        "level_air_cadangan": air
+        }
 
-try:
-    response = requests.post(URL, json=payload)
-    if response.status_code == 200:
-        data_server = response.json()
-        print("\n[✅ BERHASIL] Data diterima oleh server FastAPI!")
-        print("-" * 40)
-        print("💡 HASIL KEPUTUSAN SISTEM (SWP):")
-        print(f">> {data_server['status_rekomendasi']} <<")
-        print("-" * 40)
-    else:
-        print(f"[❌ GAGAL] Status Code: {response.status_code}")
-except Exception as e:
-    print(f"[💥 ERROR] Gagal terhubung. Pastikan server FastAPI (main.py) sudah menyala! Detail: {e}")
+        print("\nMengirim data ke server...")
+        response = requests.post(URL, json=payload)
+
+        if response.status_code == 200:
+            data_server = response.json()
+            print("\n[✅ BERHASIL] Data diterima oleh server FastAPI!")
+            print("-" * 40)
+            print("💡 HASIL KEPUTUSAN SISTEM (SWP):")
+            print(f">> {data_server['status_rekomendasi']} <<")
+            print("-" * 40)
+        else:
+            print(f"[❌ GAGAL] Status Code: {response.status_code}")
+    except ValueError:
+        print("[⚠️ ERROR] Input dibatalkan. Harap masukkan format angka yang valid (contoh: 15.5)!")
+    except Exception as e:
+        print(f"[💥 ERROR] Gagal terhubung. Pastikan server FastAPI (main.py) sudah menyala! Detail: {e}")
